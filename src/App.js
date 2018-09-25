7df5514ea784fd806b820b934bbd9a3364739d19
koch666 <julien.chollet@yahoo.fr>
2018-09-25 16:48:26 +0200

    debut jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+
 import './App.css'
+
+import GuessCount from './GuessCount'
+import Card from "./Card";
 
 class App extends Component {
     render() {
-        return <div className="memory" />
+        return (
+            <div className="memory">
+            <GuessCount guesses={0} />
+            <Card/>
+            <div/>
+        )
     }
 }
 
